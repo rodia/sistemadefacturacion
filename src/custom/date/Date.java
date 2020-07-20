f43154bbaaa7fd41384888c9ee35c185274cc22a
César Jaldin <rodia.piedra@gmail.com>
2020-07-20 15:25:19 -0400

    Update date class

--- src/custom/date/Date.java
+++ src/custom/date/Date.java
@@ -1,7 +1,5 @@
 package custom.date;
 
-import java.io.Serializable;
-
 public class Date {
     public static final String[] months = {
             "Enero",
@@ -18,20 +16,20 @@ public class Date {
             "Diciembre"
     };
 
-    public static Serializable getMonthLiteral(int month) {
+    public static String getMonthLiteral(int month) {
         if (month < 1 || month > 12) {
-            return new IllegalArgumentException("The month is not valid. it must be a number between 1 and 12.");
+            throw new IllegalArgumentException("The month is not valid. it must be a number between 1 and 12.");
         }
 
         return months[month - 1];
     }
 
-    public static Serializable getDate(int day, int month, int year) {
+    public static String getDate(int day, int month, int year) {
         if (day < 1 || day > 31) {
-            return new IllegalArgumentException("The day is not valid. it must be a number between 1 and 31.");
+            throw new IllegalArgumentException("The day is not valid. it must be a number between 1 and 31.");
         }
         if (month < 1 || month > 12) {
-            return new IllegalArgumentException("The month is not valid. It must be a number between 1 and 12.");
+            throw new IllegalArgumentException("The month is not valid. It must be a number between 1 and 12.");
         }
 
         return String.format("%d de %s de %d", day, Date.getMonthLiteral(month), year);
